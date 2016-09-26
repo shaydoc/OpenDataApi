b309296aeb31f2729cd8c7cc95afd54eb8a6a452
Shay Doherty <swampscottmusic@gmail.com>
2016-09-26 16:57:59 +0100

    INITIAL COMMIT OF API

---
+++ OpenDataApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace OpenDataApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.EnableCors();
+            // Web API configuration and services
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ OpenDataApi/Controllers/PrescriptionsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Filters;
+
+namespace OpenDataApi.Controllers
+{
+    public class CacheControlAttribute : System.Web.Http.Filters.ActionFilterAttribute
+    {
+        public int MaxAge { get; set; }
+
+        public CacheControlAttribute()
+        {
+            MaxAge = 3600;
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            if (actionExecutedContext.Response != null)
+                actionExecutedContext.Response.Headers.CacheControl = new CacheControlHeaderValue()
+                {
+                    Public = true,
+                    MaxAge = TimeSpan.FromSeconds(MaxAge)
+                };
+            base.OnActionExecuted(actionExecutedContext);
+        }
+
+         
+    }
+
+    public class PrescriptionsController : ApiController
+    {
+     
+        [CacheControl(MaxAge = 300)]
+        public dynamic Get()
+        {
+            var cacheResults = 
+                HttpRuntime.Cache["BNFChapterDrugSpending"];
+
+            if (null == cacheResults)
+            {
+                Models.MedsEntities m = new Models.MedsEntities();
+
+                var results = m.BNFChapterDrugSpendings.ToList();
+
+                HttpRuntime.Cache["BNFChapterDrugSpending"] = results;
+
+                return results;
+            }
+
+            return cacheResults;
+        }
+    }
+}
